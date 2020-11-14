1fd2a1054d51c18565e4dfac56252fadbd7cd0fd
farhaniupr <farhaniupr@gmail.com>
2020-11-14 15:12:27 +0700

    initial verifikasi

--- app/controller/handler.go
+++ app/controller/handler.go
@@ -7,8 +7,14 @@ import (
 	"log"
 	"net/http"
 
+	"gopkg.in/gomail.v2"
+
 	"github.com/gin-gonic/gin"
 )
+
+type Verif struct {
+	Email string `json:"email"`
+}
 
 func CreateAccount(c *gin.Context) {
 
@@ -92,4 +98,54 @@ func CreateUserKag(c *gin.Context) {
 		utils.WrapAPIError(c, err.Error(), http.StatusBadRequest)
 		return
 	}
+}
+
+//func main() {
+// sender configuration.
+const CONFIG_SMTP_HOST = "smtp.gmail.com"
+const CONFIG_SMTP_PORT = 587
+const CONFIG_SENDER_NAME = "CoCreate <mvpkelompok1@gmail.com>"
+const CONFIG_AUTH_EMAIL = "mvpkelompok1@gmail.com"
+const CONFIG_AUTH_PASSWORD = "14112020mvp"
+
+func verif(c *gin.Context) {
+
+	var v Verif
+	if err := c.Bind(&v); err != nil {
+		utils.WrapAPIError(c, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	mailer := gomail.NewMessage()
+	mailer.SetHeader("From", CONFIG_SENDER_NAME)
+	mailer.SetHeader("To", v.Email)
+	//mailer.SetAddressHeader("Cc", "tralalala@gmail.com", "Tra Lala La")
+	mailer.SetHeader("Subject", "Test mail")
+	mailer.SetBody("text/html", "Verifikasi email")
+	//mailer.Attach("./sample.png")
+
+	dialer := gomail.NewDialer(
+		CONFIG_SMTP_HOST,
+		CONFIG_SMTP_PORT,
+		CONFIG_AUTH_EMAIL,
+		CONFIG_AUTH_PASSWORD,
+	)
+
+	//flag, err := model.UserIKat(usk)
+	err := dialer.DialAndSend(mailer)
+	/*if flag {
+		utils.WrapAPISuccess(c, "success", http.StatusOK)
+		return
+	} else {
+		utils.WrapAPIError(c, err.Error(), http.StatusBadRequest)
+		return
+	}*/
+
+	if err != nil {
+		log.Fatal(err.Error())
+	} else {
+		utils.WrapAPISuccess(c, "success", http.StatusOK)
+		log.Println("Mail sent!")
+	}
+
 }
