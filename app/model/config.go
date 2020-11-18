daf3cddca19fcbb898fac98ccae4cba50248494f
Farhani Adul <farhaniupr@gmail.com>
2020-11-18 17:43:16 +0000

    Update config.go

--- app/model/config.go
+++ app/model/config.go
@@ -1,7 +1,7 @@
 package model
 
 import (
-	//"fmt"
+	"fmt"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -16,15 +16,15 @@ const (
 func init() {
 	var err error
 
-	//db_u := "user"           //os.Getenv("DB_U")
-	//db_p := ""               //os.Getenv("DB_P")
-	//db_host := "3.15.137.94" //os.Getenv("DB_HOST")
-	//db_name := "bank"        //os.Getenv("DB_NAME")
+	db_u := "user"           //os.Getenv("DB_U")
+	db_p := ""               //os.Getenv("DB_P")
+	db_host := "3.15.137.94" //os.Getenv("DB_HOST")
+	db_name := "bank"        //os.Getenv("DB_NAME")
 
 	// DB, err = gorm.Open(mysql.Open(fmt.Sprintf("root:root@tcp(172.18.0.10:3306)/digitalent_bank")), &gorm.Config{})
 	for {
-		//DB, err = gorm.Open(mysql.Open(fmt.Sprintf("%s:%s@tcp(%s)/%s", db_u, db_p, db_host, db_name)), &gorm.Config{})
-		DB, err = gorm.Open(mysql.Open(MysqlDsn), &gorm.Config{})
+		DB, err = gorm.Open(mysql.Open(fmt.Sprintf("%s:%s@tcp(%s)/%s", db_u, db_p, db_host, db_name)), &gorm.Config{})
+		//DB, err = gorm.Open(mysql.Open(MysqlDsn), &gorm.Config{})
 		if err == nil {
 			break
 		}
